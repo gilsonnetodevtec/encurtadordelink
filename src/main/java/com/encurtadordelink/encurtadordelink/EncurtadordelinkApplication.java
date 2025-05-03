e532107aea0860186155ae65b9428627185978f1
gilsonnetodevtec <gilsonnetodevtec@gmail.com>
2025-05-03 19:38:07 -0300

    feat: Inicio o projeto spring

---
+++ src/main/java/com/encurtadordelink/encurtadordelink/EncurtadordelinkApplication.java
@@ -0,0 +1,13 @@
+package com.encurtadordelink.encurtadordelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadordelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadordelinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encurtadordelink/encurtadordelink/EncurtadordelinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encurtadordelink.encurtadordelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadordelinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
