3dc3d3a3d4d258c633f0c1e4490d5299b2aad2b8
gilsonnetodevtec <gilsonnetodevtec@gmail.com>
2025-05-03 21:19:23 -0300

    feat: "Entidade corrigido e melhorado"

--- src/main/java/com/encurtadordelink/encurtadordelink/Entity/Link.java
+++ src/main/java/com/encurtadordelink/encurtadordelink/Entity/Link.java
@@ -1,6 +1,9 @@
 package com.encurtadordelink.encurtadordelink.Entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
@@ -15,9 +18,15 @@ import lombok.Setter;
 public class Link {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
     private String uid;
 
+    @Column(nullable = false)
     private String urlOriginal;
 
+    @Column(nullable = false)
     private String urlEncurtada;
 }
